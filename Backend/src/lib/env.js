072b6a0e443f31c59a9e5769bfbeb236bdad136f
devel-maverick <codemaverick00@gmail.com>
2025-12-02 19:18:13 +0530

    new env changes

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -1,9 +1,5 @@
 import 'dotenv/config';
 export const ENV={
-PORT:process.env.PORT	,
-JWT_SECRET:process.env.JWT_SECRET,
-NODE_ENV:process.env.NODE_ENV	,
-CLIENT_URL:process.env.CLIENT_URL	,
 CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
 CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
 CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET

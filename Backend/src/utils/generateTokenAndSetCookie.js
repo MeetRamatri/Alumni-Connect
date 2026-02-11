6c21529818a9619a623b08d3173a2419292de256
MeetRamatri <meetramatri12@gmail.com>
2026-02-11 14:55:09 +0530

    fix cookie options for chatting

--- Backend/src/utils/generateTokenAndSetCookie.js
+++ Backend/src/utils/generateTokenAndSetCookie.js
@@ -6,8 +6,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
 
